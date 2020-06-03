d39ccd0ffe678623902cf1791c680efd4d1e9fd3
Osahon Okungbowa <osahon.okungbowa@datopian.com>
2020-06-03 20:28:51 +0100

    octae merged 2

--- edscrapers/scrapers/edgov/octae/parsers/__init__.py
+++ edscrapers/scrapers/edgov/octae/parsers/__init__.py
@@ -1,3 +1,2 @@
 # import modules from this package with simplier names
-from edscrapers.scrapers.octae.parsers import octae_parser1 as parser1
-from edscrapers.scrapers.octae.parsers import octae_parser2 as parser2
+
