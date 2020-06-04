fd3737bfbc8ac4242c04a7b22198960fe8577a37
Osahon Okungbowa <osahon.okungbowa@datopian.com>
2020-06-04 13:18:59 +0100

    merged

--- edscrapers/scrapers/edgov/oese/parser.py
+++ edscrapers/scrapers/edgov/oese/parser.py
@@ -10,12 +10,12 @@ import bs4
 
 from edscrapers.scrapers import base
 import edscrapers.scrapers.base.parser as base_parser
-from edscrapers.scrapers.oese import parsers
+from edscrapers.scrapers.edgov.oese import parsers
 
 # contains list of data resources to exclude from dataset
 deny_list = []
 
-def parse(res):
+def parse(res, publisher={'name': 'oese', 'subOrganizationOf': None}):
     """ function parses content to create a dataset model
     or return None if no resource in content"""
 
@@ -39,7 +39,7 @@ def parse(res):
     # check if the parser is working on OESE web page
     if soup_parser.body.find(name='div', class_='container', recursive=True) is not None:
         # parse the page with the parser and return result
-        return parsers.parser1.parse(res)
+        return parsers.parser1.parse(res, publisher)
     else:
         return None
 
