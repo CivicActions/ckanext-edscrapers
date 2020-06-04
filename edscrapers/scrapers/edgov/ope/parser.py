3c8aa68ada2216668bfc1512a7344bd6aa214bde
Osahon Okungbowa <osahon.okungbowa@datopian.com>
2020-06-04 13:32:56 +0100

    merged

--- edscrapers/scrapers/edgov/ope/parser.py
+++ edscrapers/scrapers/edgov/ope/parser.py
@@ -10,12 +10,12 @@ import bs4
 
 from edscrapers.scrapers import base
 import edscrapers.scrapers.base.parser as base_parser
-from edscrapers.scrapers.ope import parsers
+from edscrapers.scrapers.edgov.ope import parsers
 
 # contains list of data resources to exclude from dataset
 deny_list = []
 
-def parse(res):
+def parse(res, publisher={'name': 'ope', 'subOrganizationOf': None}):
     """ function parses content to create a dataset model
     or return None if no resource in content"""
 
@@ -31,11 +31,11 @@ def parse(res):
     # check if the parser is working on OPE web page
     if soup_parser.body.find(name='div', id='maincontent', recursive=True) is not None:
         # parse the page with the parser and return result
-        return parsers.parser1.parse(res)
+        return parsers.parser1.parse(res=res, publisher=publisher)
     # check if the parser is working on OCTAE web page (variant 2)
     if soup_parser.body.select_one('.headersLevel1') is not None:
         # parse the page with the parser and return result
-        return parsers.parser2.parse(res)
+        return parsers.parser2.parse(res=res, publisher=publisher)
     else:
         return None
 
