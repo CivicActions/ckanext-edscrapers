d84feeabf2ef022d8ec2fc4a173cd9151879f9f1
Osahon Okungbowa <osahon.okungbowa@datopian.com>
2020-06-04 14:16:34 +0100

    merged

--- edscrapers/scrapers/edgov/osers/parser.py
+++ edscrapers/scrapers/edgov/osers/parser.py
@@ -5,11 +5,11 @@ from datetime import datetime
 
 from edscrapers.cli import logger
 import edscrapers.scrapers.base.parser as base_parser
-from edscrapers.scrapers.osers import parsers
+from edscrapers.scrapers.edgov.osers import parsers
 
 deny_list = []
 
-def parse(res):
+def parse(res, publisher={'name': 'osers', 'subOrganizationOf': None}):
 
     """ function parses content to create a dataset model
     or return None if no resource in content"""
@@ -34,21 +34,15 @@ def parse(res):
     # check if the parser is working on OSERS web page
     if soup_parser.body.find(name='div', id='maincontent', recursive=True) is not None:
         # parse the page with the parser and return result
-        return parsers.parser1.parse(res)
+        return parsers.parser1.parse(res, publisher)
 
     # check if the parser is working on OCTAE web page (variant 2)
     if soup_parser.body.select_one('.headersLevel1') is not None:
         # parse the page with the parser and return result
-        return parsers.parser2.parse(res)
+        return parsers.parser2.parse(res, publisher)
     else:
 
         logger.error('Page doesnt fit in any structure:')
         logger.error(res)
 
         return None
-
-        
-
-    
-
-    
